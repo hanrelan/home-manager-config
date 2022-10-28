6030f31635830e1ddbadca6d690b8dfc62f4c76d
Rohan Relan <roresemail@gmail.com>
2022-10-28 15:32:31 -0700

    Use xfce-power-manager, fix brightness script

--- herbstluftwm/polybar-config/brightness.sh
+++ herbstluftwm/polybar-config/brightness.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    printf '%s' "$var"
+}
+
+UPDATE="polybar-msg hook brightnesscontrol 1"
+
 case $1 in
 	get)
 		BRIGHTNESS1=`ddcutil getvcp 10 --display 1 | awk '{print $9}' | sed 's/,//'`
@@ -11,10 +22,29 @@ case $1 in
 	increase)
 		ddcutil setvcp 10 + 10 --display 1
 		ddcutil setvcp 10 + 10 --display 2
+		eval "$UPDATE"
 	;;
 
 	decrease)
 		ddcutil setvcp 10 - 10 --display 1
 		ddcutil setvcp 10 - 10 --display 2
+		eval "$UPDATE"
+	;;
+	set)
+		result=$(echo "" | rofi -no-config -no-lazy-grab -theme ~/.config/polybar/cuts/scripts/rofi/message.rasi -dmenu -p "New brightness: ")
+		if [[ $result == *"/"* ]]; then
+			# format is 50 / 50
+			 IFS='/'
+
+			read -a strarr <<< "$result"
+			b1=$(echo ${strarr[0]} | xargs)
+			b2=$(echo ${strarr[1]} | xargs)
+			ddcutil setvcp 10 $b1 --display 1
+			ddcutil setvcp 10 $b2 --display 2
+		else
+			ddcutil setvcp 10 $result --display 1
+			ddcutil setvcp 10 $result --display 2
+		fi
+		eval "$UPDATE"
 	;;
 esac
