20e064b4b9f8f9108e3aeb46a3b34f29ec2d1f50
Rohan Relan <roresemail@gmail.com>
2023-03-20 13:43:34 -0700

    Add install instructions

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 mkdir -p ${HOME}/.config/home-manager
 ln -s ${PWD}/home.nix ${HOME}/.config/home-manager/home.nix
+
+echo "Make sure the nix shells are in /etc/shells"
+echo "Install miniconda to ${HOME}/miniconda3"
